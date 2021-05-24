985490f1b03d1a60430641dad2877e055fba846d
MatiasCeb <matiasceb8812@gmail.com>
2021-05-23 22:53:32 -0300

    class 4

---
+++ src/components/Characters.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+
+const Characters = () => {
+
+    
+
+    return (
+        <div className="Characters">
+            <h2>{}</h2>
+        </div>
+    );
+}
+
+export default Characters;
